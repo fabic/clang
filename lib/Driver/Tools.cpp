9e8bf93e67fb3ec2ec0d5a0667ca51810b3130ca
fcj <fabic@laptop.RandomBinaryThoughts.com>
2014-09-24 15:38:40 +0200

    FABIC: Changed /lib/ld-linux... for ld-hack... (/me was playing with musl libc a few days ago)
    (cherry picked from commit 652cf6f1160f21b3e9b78d85866fe62fe8dda00f)

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -8029,9 +8029,9 @@ static std::string getLinuxDynamicLinker(const ArgList &Args,
     return "/lib64/ld-linux.so.2";
   else if (Arch == llvm::Triple::x86_64 &&
            ToolChain.getTriple().getEnvironment() == llvm::Triple::GNUX32)
-    return "/libx32/ld-linux-x32.so.2";
+    return "/lib32/ld-hack-x32.so";
   else
-    return "/lib64/ld-linux-x86-64.so.2";
+    return "/lib/ld-hack-x86_64.so";
 }
 
 static void AddRunTimeLibs(const ToolChain &TC, const Driver &D,

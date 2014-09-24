510ca5244672b06a1a0600143db6f6fe09f0149a
fcj <fabic@laptop.RandomBinaryThoughts.com>
2014-09-24 15:39:39 +0200

    REVERT "FABIC: Changed /lib/ld-linux... for ld-hack...
    This reverts commit 652cf6f1160f21b3e9b78d85866fe62fe8dda00f.

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -7226,9 +7226,9 @@ static StringRef getLinuxDynamicLinker(const ArgList &Args,
     return "/lib64/ld-linux.so.2";
   else if (ToolChain.getArch() == llvm::Triple::x86_64 &&
            ToolChain.getTriple().getEnvironment() == llvm::Triple::GNUX32)
-    return "/lib32/ld-hack-x32.so";
+    return "/libx32/ld-linux-x32.so.2";
   else
-    return "/lib/ld-hack-x86_64.so";
+    return "/lib64/ld-linux-x86-64.so.2";
 }
 
 static void AddRunTimeLibs(const ToolChain &TC, const Driver &D,
